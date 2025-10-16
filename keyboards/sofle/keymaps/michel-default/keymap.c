d5e486571fc054c937953175446c7c9a7790fd9c
Michel Philippe Edubas <mikeedubas@hotmail.com>
2025-10-17 08:35:37 +1000

    some updates

--- keyboards/sofle/keymaps/michel-default/keymap.c
+++ keyboards/sofle/keymaps/michel-default/keymap.c
@@ -4,11 +4,15 @@
 
 #define SPC_NAV  LT(_NAV, KC_SPC)
 #define ENT_NUM  LT(_NUM, KC_ENT)
-#define BCSP_SYM  LT(_SYM, KC_BSPC)
+// #define BCSP_SYM  LT(_SYM, KC_BSPC)
 
 #define ESC_GUI  MT(MOD_LGUI, KC_ESC)
 #define SCLN_GUI MT(MOD_RGUI, KC_SCLN)
-#define EQUAL_SHFT  MT(MOD_LSFT, KC_EQUAL)
+
+#define BCSP_SHFT  MT(MOD_RSFT, KC_BSPC)
+// #define CBCSP_SHFT  MT(MOD_RSFT, LCTL(KC_BSPC))
+
+// #define EQUAL_SHFT  MT(MOD_LSFT, KC_EQUAL)
 
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
@@ -21,9 +25,10 @@ enum sofle_layers {
 };
 
 enum custom_keycodes {
-    TMUX_SHFT = SAFE_RANGE,
+    // TMUX_SHFT = SAFE_RANGE,
     // QUOTE_GRV,
-    MOU_BSPC,
+    // MOU_BSPC,
+    SHFT_BSPC = SAFE_RANGE
 };
 
 typedef struct {
@@ -37,24 +42,25 @@ typedef struct {
 //     uint16_t timer;
 // } single_state_t;
 
-static dual_state_t tmux = {0};
-static dual_state_t mb = {0};
+static dual_state_t shft = {0};
+// static dual_state_t tmux = {0};
+// static dual_state_t mb = {0};
 // static single_state_t qg = {0};
 
 const uint16_t PROGMEM left_ctrl[] = {KC_S, KC_T, COMBO_END};
 const uint16_t PROGMEM left_alt[] = {KC_T, KC_D, COMBO_END};
-const uint16_t PROGMEM left_sft[] = {KC_M, KC_E, COMBO_END};
+// const uint16_t PROGMEM left_sft[] = {KC_M, KC_E, COMBO_END};
 const uint16_t PROGMEM right_ctrl[] = {KC_N, KC_E, COMBO_END};
 const uint16_t PROGMEM right_alt[] = {KC_N, KC_H, COMBO_END};
-const uint16_t PROGMEM right_sft[] = {KC_V, KC_S, COMBO_END};
+// const uint16_t PROGMEM right_sft[] = {KC_V, KC_S, COMBO_END};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
       KC_CAPS,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
       KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT,  KC_BSLS,
       ESC_GUI,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  SCLN_GUI,
-      TMUX_SHFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  EQUAL_SHFT,
-                     KC_LCTL, KC_LALT, _______, MOU_BSPC, SPC_NAV,        ENT_NUM,  BCSP_SYM, _______, KC_RALT, KC_RCTL
+      LCTL(KC_B),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQUAL,
+                     KC_LCTL, KC_LALT, MO(_MOU), SHFT_BSPC, SPC_NAV,        ENT_NUM,  BCSP_SHFT, MO(_SYM), KC_RALT, KC_RCTL
     ),
     [_NAV] = LAYOUT(
       _______, LSFT(KC_F2), LSFT(KC_F3), LSFT(KC_F4), _______, _______,                   KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
@@ -89,75 +95,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 combo_t key_combos[] = {
     COMBO(left_ctrl, KC_LCTL),
     COMBO(left_alt, KC_LALT),
-    COMBO(left_sft, KC_LSFT),
+    // COMBO(left_sft, KC_LSFT),
     COMBO(right_ctrl, KC_RCTL),
     COMBO(right_alt, KC_RALT),
-    COMBO(right_sft, KC_RSFT),
+    // COMBO(right_sft, KC_RSFT),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case TMUX_SHFT:
+        case SHFT_BSPC:
             if (record->event.pressed) {
-                tmux.down  = true;
-                tmux.hold  = false;
-                tmux.timer = timer_read();
+                shft.down  = true;
+                shft.hold  = false;
+                shft.timer = timer_read();
             } else {
-                if (!tmux.hold && timer_elapsed(tmux.timer) < TAPPING_TERM) {
-                    register_mods(MOD_BIT(KC_LCTL));
-                    tap_code(KC_B);
-                    unregister_mods(MOD_BIT(KC_LCTL));
+                if (!shft.hold && timer_elapsed(shft.timer) < TAPPING_TERM) {
+                    // TAP: Ctrl+Backspace
+                    // Option A (cleaner):
+                    tap_code16(LCTL(KC_BSPC));
+                    // Option B (what you had, also fine):
+                    // register_mods(MOD_BIT(KC_LCTL));
+                    // tap_code(KC_BSPC);
+                    // unregister_mods(MOD_BIT(KC_LCTL));
                 } else {
-                    if (tmux.hold) {
-                        unregister_mods(MOD_BIT(KC_RSFT));
+                    // HOLD: stop holding Shift when released
+                    if (shft.hold) {
+                        unregister_mods(MOD_BIT(KC_LSFT));  // not tap_code
                     }
                 }
-                tmux.down = false;
+                shft.down = false;
             }
             return false; // handled
 
-        // case QUOTE_GRV:
-        //     if (record->event.pressed) {
-        //         qg.down  = true;
-        //         qg.timer = timer_read();
-        //     } else {
-        //         if (timer_elapsed(qg.timer) < TAPPING_TERM) {
-        //             tap_code(KC_QUOT);
-        //         } else {
-        //             tap_code(KC_GRV);
-        //         }
-        //         qg.down = false;
-        //     }
-        //     return false; // handled
-
-        case MOU_BSPC:
-            if (record->event.pressed) {
-                mb.down = true;
-                mb.hold = false;
-                mb.timer = timer_read();
-            } else {
-                if (!mb.hold && timer_elapsed(mb.timer) < TAPPING_TERM) {
-                    register_mods(MOD_BIT(KC_LCTL));
-                    tap_code(KC_BSPC);
-                    unregister_mods(MOD_BIT(KC_LCTL));
-                } else {
-                    if (mb.hold) {
-                        layer_off(_MOU);
-                    }
-                }
-                mb.down = false;
-            }
-            return false;
-
         default:
-            // Convert to HOLD the moment another key is pressed while the key is down
+            // Convert to HOLD the moment another key is pressed while SHFT_BSPC is down
             if (record->event.pressed) {
-                if (tmux.down && !tmux.hold) {
-                    register_mods(MOD_BIT(KC_RSFT));
-                    tmux.hold = true;
-                } else if (mb.down && !mb.hold) {
-                    layer_on(_MOU);
-                    mb.hold = true;
+                if (shft.down && !shft.hold) {
+                    register_mods(MOD_BIT(KC_LSFT)); // start holding Shift
+                    shft.hold = true;
                 }
             }
             return true; // let other keys process normally
