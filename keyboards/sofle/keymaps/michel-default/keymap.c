d823c4103c3b340c3a82e6ff3bd9cb064b9c1f04
Michel Philippe Edubas <mikeedubas@hotmail.com>
2025-10-23 07:31:24 +1000

    work in progress

--- keyboards/sofle/keymaps/michel-default/keymap.c
+++ keyboards/sofle/keymaps/michel-default/keymap.c
@@ -1,140 +1,128 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 #include QMK_KEYBOARD_H
 
-#define SPC_NAV  LT(_NAV, KC_SPC)
-#define ENT_NUM  LT(_NUM, KC_ENT)
-// #define BCSP_SYM  LT(_SYM, KC_BSPC)
+#include "manna-harbour_miryoku.h"
 
-#define ESC_GUI  MT(MOD_LGUI, KC_ESC)
-#define SCLN_GUI MT(MOD_RGUI, KC_SCLN)
 
-#define BCSP_SHFT  MT(MOD_RSFT, KC_BSPC)
-// #define CBCSP_SHFT  MT(MOD_RSFT, LCTL(KC_BSPC))
+// Additional Features double tap guard
 
-// #define EQUAL_SHFT  MT(MOD_LSFT, KC_EQUAL)
-
-enum sofle_layers {
-    /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
-    _BASE,
-    _NUM,
-    _SYM,
-    _FUN,
-    _NAV,
-    _MOU,
+enum {
+    U_TD_BOOT,
+#define MIRYOKU_X(LAYER, STRING) U_TD_U_##LAYER,
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
 };
 
-enum custom_keycodes {
-    // TMUX_SHFT = SAFE_RANGE,
-    // QUOTE_GRV,
-    // MOU_BSPC,
-    SHFT_BSPC = SAFE_RANGE
+void u_td_fn_boot(tap_dance_state_t *state, void *user_data) {
+  if (state->count == 2) {
+    reset_keyboard();
+  }
+}
+
+#define MIRYOKU_X(LAYER, STRING) \
+void u_td_fn_U_##LAYER(tap_dance_state_t *state, void *user_data) { \
+  if (state->count == 2) { \
+    default_layer_set((layer_state_t)1 << U_##LAYER); \
+  } \
+}
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
+
+tap_dance_action_t tap_dance_actions[] = {
+    [U_TD_BOOT] = ACTION_TAP_DANCE_FN(u_td_fn_boot),
+#define MIRYOKU_X(LAYER, STRING) [U_TD_U_##LAYER] = ACTION_TAP_DANCE_FN(u_td_fn_U_##LAYER),
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
 };
 
-typedef struct {
-    bool     down;
-    bool     hold;
-    uint16_t timer;
-} dual_state_t;
 
-// typedef struct {
-//     bool     down;
-//     uint16_t timer;
-// } single_state_t;
-
-static dual_state_t shft = {0};
-// static dual_state_t tmux = {0};
-// static dual_state_t mb = {0};
-// static single_state_t qg = {0};
-
-const uint16_t PROGMEM left_ctrl[] = {KC_S, KC_T, COMBO_END};
-const uint16_t PROGMEM left_alt[] = {KC_T, KC_D, COMBO_END};
-// const uint16_t PROGMEM left_sft[] = {KC_M, KC_E, COMBO_END};
-const uint16_t PROGMEM right_ctrl[] = {KC_N, KC_E, COMBO_END};
-const uint16_t PROGMEM right_alt[] = {KC_N, KC_H, COMBO_END};
-// const uint16_t PROGMEM right_sft[] = {KC_V, KC_S, COMBO_END};
+// keymap
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT(
-      KC_CAPS,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
-      KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT,  KC_BSLS,
-      ESC_GUI,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  SCLN_GUI,
-      LCTL(KC_B),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQUAL,
-                     KC_LCTL, KC_LALT, MO(_MOU), SHFT_BSPC, SPC_NAV,        ENT_NUM,  BCSP_SHFT, MO(_SYM), KC_RALT, KC_RCTL
-    ),
-    [_NAV] = LAYOUT(
-      _______, LSFT(KC_F2), LSFT(KC_F3), LSFT(KC_F4), _______, _______,                   KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-      _______, LSFT(KC_F5), KC_F5, KC_F9, KC_F10, KC_F11,                   KC_PSCR, _______, _______, _______, _______, _______,
-      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_BSPC,                   _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,
-      _______, _______, _______, LCTL(KC_F7), KC_F12, LCTL(KC_F12), _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______,
-                         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-    [_SYM] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-      _______, KC_LBRC, KC_AMPR, KC_ASTR, KC_GRV, KC_RBRC,                   _______, _______, _______, _______, _______, _______,
-      _______, KC_LCBR, KC_DLR, KC_PERC, KC_CIRC, KC_RCBR,                   KC_BSPC, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, _______,
-      _______, KC_LPRN,  KC_EXLM, KC_AT, KC_HASH, KC_RPRN, _______, _______, _______, _______, _______, _______, _______,  _______,
-                         _______, _______, KC_PLUS, KC_MINS, KC_UNDS, _______, _______, _______, _______, _______
-    ),
-    [_NUM] = LAYOUT(
-      KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5, KC_F6,                   _______, _______, _______, _______, _______, _______,
-      _______, KC_SLSH, KC_7, KC_8, KC_9, KC_PLUS,                   _______, _______, _______, _______, _______, _______,
-      _______, KC_ASTR, KC_4, KC_5, KC_6, KC_MINS,                   KC_BSPC, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, _______,
-      _______, KC_DOT,  KC_1, KC_2, KC_3, KC_EQUAL, _______, _______, _______, _______, _______, _______, _______,  _______,
-                         _______, _______, _______, KC_0, _______, _______, _______, _______, _______, _______
-    ),
-    [_MOU] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_BSPC,                   _______, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, _______,
-                         _______, _______, _______, _______, _______, MS_BTN1, MS_BTN2, MS_BTN3, _______, _______
-    ),
+#define MIRYOKU_X(LAYER, STRING) [U_##LAYER] = U_MACRO_VA_ARGS(MIRYOKU_LAYERMAPPING_##LAYER, MIRYOKU_LAYER_##LAYER),
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
 };
 
-combo_t key_combos[] = {
-    COMBO(left_ctrl, KC_LCTL),
-    COMBO(left_alt, KC_LALT),
-    // COMBO(left_sft, KC_LSFT),
-    COMBO(right_ctrl, KC_RCTL),
-    COMBO(right_alt, KC_RALT),
-    // COMBO(right_sft, KC_RSFT),
+
+// shift functions
+
+const key_override_t capsword_key_override = ko_make_basic(MOD_MASK_SHIFT, CW_TOGG, KC_CAPS);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &capsword_key_override,
+    NULL
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case SHFT_BSPC:
-            if (record->event.pressed) {
-                shft.down  = true;
-                shft.hold  = false;
-                shft.timer = timer_read();
-            } else {
-                if (!shft.hold && timer_elapsed(shft.timer) < TAPPING_TERM) {
-                    // TAP: Ctrl+Backspace
-                    // Option A (cleaner):
-                    tap_code16(LCTL(KC_BSPC));
-                    // Option B (what you had, also fine):
-                    // register_mods(MOD_BIT(KC_LCTL));
-                    // tap_code(KC_BSPC);
-                    // unregister_mods(MOD_BIT(KC_LCTL));
-                } else {
-                    // HOLD: stop holding Shift when released
-                    if (shft.hold) {
-                        unregister_mods(MOD_BIT(KC_LSFT));  // not tap_code
-                    }
-                }
-                shft.down = false;
-            }
-            return false; // handled
 
-        default:
-            // Convert to HOLD the moment another key is pressed while SHFT_BSPC is down
-            if (record->event.pressed) {
-                if (shft.down && !shft.hold) {
-                    register_mods(MOD_BIT(KC_LSFT)); // start holding Shift
-                    shft.hold = true;
-                }
-            }
-            return true; // let other keys process normally
-    }
-}
+// thumb combos
+
+#if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)
+const uint16_t PROGMEM thumbcombos_base_right[] = {LT(U_SYM, KC_ENT), LT(U_NUM, KC_BSPC), COMBO_END};
+const uint16_t PROGMEM thumbcombos_base_left[] = {LT(U_NAV, KC_SPC), LT(U_MOUSE, KC_TAB), COMBO_END};
+const uint16_t PROGMEM thumbcombos_nav[] = {KC_ENT, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM thumbcombos_mouse[] = {KC_BTN2, KC_BTN1, COMBO_END};
+const uint16_t PROGMEM thumbcombos_media[] = {KC_MSTP, KC_MPLY, COMBO_END};
+const uint16_t PROGMEM thumbcombos_num[] = {KC_0, KC_MINS, COMBO_END};
+  #if defined (MIRYOKU_LAYERS_FLIP)
+const uint16_t PROGMEM thumbcombos_sym[] = {KC_UNDS, KC_LPRN, COMBO_END};
+  #else
+const uint16_t PROGMEM thumbcombos_sym[] = {KC_RPRN, KC_UNDS, COMBO_END};
+  #endif
+const uint16_t PROGMEM thumbcombos_fun[] = {KC_SPC, KC_TAB, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+  COMBO(thumbcombos_base_right, LT(U_FUN, KC_DEL)),
+  COMBO(thumbcombos_base_left, LT(U_MEDIA, KC_ESC)),
+  COMBO(thumbcombos_nav, KC_DEL),
+  COMBO(thumbcombos_mouse, KC_BTN3),
+  COMBO(thumbcombos_media, KC_MUTE),
+  COMBO(thumbcombos_num, KC_DOT),
+  #if defined (MIRYOKU_LAYERS_FLIP)
+  COMBO(thumbcombos_sym, KC_RPRN),
+  #else
+  COMBO(thumbcombos_sym, KC_LPRN),
+  #endif
+  COMBO(thumbcombos_fun, KC_APP)
+};
+
+// #endifconst uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//     [_BASE] = LAYOUT(
+//       _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______,  _______,
+//       _______,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT,  _______,
+//       _______,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  _______,
+//       _______,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  _______,
+//                      _______, _______, MO(_MOU), SHFT_BSPC, SPC_NAV,        ENT_NUM,  BCSP_SHFT, MO(_SYM), _______,_______
+//     ),
+//     [_NAV] = LAYOUT(
+//       _______, LSFT(KC_F2), LSFT(KC_F3), LSFT(KC_F4), _______, _______,                   KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+//       _______, LSFT(KC_F5), KC_F5, KC_F9, KC_F10, KC_F11,                   KC_PSCR, _______, _______, _______, _______, _______,
+//       _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_BSPC,                   _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,
+//       _______, _______, _______, LCTL(KC_F7), KC_F12, LCTL(KC_F12), _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______,
+//                          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+//     ),
+//     [_SYM] = LAYOUT(
+//       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+//       _______, KC_LBRC, KC_AMPR, KC_ASTR, KC_GRV, KC_RBRC,                   _______, _______, _______, _______, _______, _______,
+//       _______, KC_LCBR, KC_DLR, KC_PERC, KC_CIRC, KC_RCBR,                   KC_BSPC, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, _______,
+//       _______, KC_LPRN,  KC_EXLM, KC_AT, KC_HASH, KC_RPRN, _______, _______, _______, _______, _______, _______, _______,  _______,
+//                          _______, _______, KC_PLUS, KC_MINS, KC_UNDS, _______, _______, _______, _______, _______
+//     ),
+//     [_NUM] = LAYOUT(
+//       KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5, KC_F6,                   _______, _______, _______, _______, _______, _______,
+//       _______, KC_SLSH, KC_7, KC_8, KC_9, KC_PLUS,                   _______, _______, _______, _______, _______, _______,
+//       _______, KC_ASTR, KC_4, KC_5, KC_6, KC_MINS,                   KC_BSPC, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, _______,
+//       _______, KC_DOT,  KC_1, KC_2, KC_3, KC_EQUAL, _______, _______, _______, _______, _______, _______, _______,  _______,
+//                          _______, _______, _______, KC_0, _______, _______, _______, _______, _______, _______
+//     ),
+//     [_MOU] = LAYOUT(
+//       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+//       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+//       _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_BSPC,                   _______, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, _______,
+//       _______, _______, _______, _______, _______, _______, _______, _______, _______, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, _______,
+//                          _______, _______, _______, _______, _______, MS_BTN1, MS_BTN2, MS_BTN3, _______, _______
+//     ),
+// };
