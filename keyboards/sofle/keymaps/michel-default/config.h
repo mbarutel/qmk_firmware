5b859cbe7dca7464d6f81e7cd14d4f816360c3da
Michel Philippe Edubas <mikeedubas@hotmail.com>
2025-10-27 09:16:51 +1000

    auto shift activated and it does not seem so bad at all

--- keyboards/sofle/keymaps/michel-default/config.h
+++ keyboards/sofle/keymaps/michel-default/config.h
@@ -19,3 +19,20 @@
 // Optional: combo config
 // #define COMBO_COUNT 2
 // #define COMBO_TERM 30
+
+// Auto Shift
+// #define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT 200
+// #define AUTO_SHIFT_NO_SETUP
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
