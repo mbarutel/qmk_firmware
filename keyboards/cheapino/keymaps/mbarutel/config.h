b39f0206dc5d9853b8471185bef5e013bd9cfb9d
Michel Philippe Edubas <mikeedubas@hotmail.com>
2025-11-26 19:52:50 +1000

    cheapino setup

---
+++ keyboards/cheapino/keymaps/mbarutel/config.h
@@ -0,0 +1,38 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 140
+#define QUICK_TAP_TERM 0
+// #define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+// #define IGNORE_MOD_TAP_INTERRUPT
+// #define TAPPING_FORCE_HOLD
+// #define RETRO_TAPPING
+
+
+// Optional: combo config
+#define COMBO_COUNT 7
+#define COMBO_TERM 20
+
+// Auto Shift
+// #define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT 130
+#define NO_AUTO_SHIFT_NUMERIC
+#define AUTO_SHIFT_TIMEOUT_PER_KEY
+// #define AUTO_SHIFT_NO_SETUP
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
