af72bf2d321b6f9a104edddc5128f66b7b727f10
Michel Philippe Edubas <mikeedubas@hotmail.com>
2025-12-05 12:02:09 +1000

    added control of the encoder

---
+++ keyboards/cheapino/keymaps/mbarutel/encoder.c
@@ -0,0 +1,64 @@
+#include "matrix.h"
+#include "quantum.h"
+
+#define COL_SHIFTER ((uint16_t)1)
+
+#define ENC_ROW 3
+#define ENC_A_COL 2
+#define ENC_B_COL 4
+#define ENC_BUTTON_COL 0
+
+static bool colABPressed   = false;
+static bool encoderPressed = false;
+
+void clicked(void) {
+    tap_code(KC_MUTE);
+}
+
+void turned(bool clockwise) {
+    if (IS_LAYER_ON(2)) {
+        // _MOU layer: mouse scroll
+        tap_code(clockwise ? KC_MS_WH_UP : KC_MS_WH_DOWN);
+    } else if (IS_LAYER_ON(6)) {
+        // _NAV layer: arrow keys
+        tap_code(clockwise ? KC_UP : KC_DOWN);
+    } else {
+        // Default: volume control
+        tap_code(clockwise ? KC_VOLU : KC_VOLD);
+    }
+}
+
+void fix_encoder_action(matrix_row_t current_matrix[]) {
+    matrix_row_t encoder_row = current_matrix[ENC_ROW];
+
+    if (encoder_row & (COL_SHIFTER << ENC_BUTTON_COL)) {
+        encoderPressed = true;
+    } else {
+        // Only trigger click on release
+        if (encoderPressed) {
+            encoderPressed = false;
+            clicked();
+        }
+    }
+
+    // Check which way the encoder is turned:
+    bool colA = encoder_row & (COL_SHIFTER << ENC_A_COL);
+    bool colB = encoder_row & (COL_SHIFTER << ENC_B_COL);
+
+    if (colA && colB) {
+        colABPressed = true;
+    } else if (colA) {
+        if (colABPressed) {
+            // A+B followed by A means clockwise
+            colABPressed = false;
+            turned(true);
+        }
+    } else if (colB) {
+        if (colABPressed) {
+            // A+B followed by B means counter-clockwise
+            colABPressed = false;
+            turned(false);
+        }
+    }
+    current_matrix[ENC_ROW] = 0;
+}
