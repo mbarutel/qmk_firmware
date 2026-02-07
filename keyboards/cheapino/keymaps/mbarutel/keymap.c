6fff62607b23ef3492cd7cf8a8ca870861e3879f
Michel Philippe Edubas <mikeedubas@hotmail.com>
2026-02-07 13:03:15 +1000

    update

--- keyboards/cheapino/keymaps/mbarutel/keymap.c
+++ keyboards/cheapino/keymaps/mbarutel/keymap.c
@@ -217,7 +217,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_MOU] = LAYOUT_split_3x5_3(
-       RGUI(KC_G), RGUI(KC_SCLN), RGUI(KC_TAB),TD(TD_E_LEFT), TD(TD_I_RIGHT),              _______, _______, _______, _______, _______,
+       RGUI(KC_Q), RGUI(KC_TAB), RGUI(KC_F),TD(TD_E_LEFT), TD(TD_I_RIGHT),              _______, _______, _______, _______, _______,
        KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, LCTL(KC_B),                    _______, MS_LEFT, MS_DOWN, MS_UP,  MS_RGHT,
        _______,_______,_______,_______,_______,                      _______, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR,
                                   _______, _______, _______,    MS_BTN1, MS_BTN2, MS_BTN3
